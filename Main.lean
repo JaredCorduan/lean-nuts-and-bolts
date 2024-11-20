67f4af111d4bb024b27d7d2802260540cea81c18
Jared Corduan <jaredcorduan@gmail.com>
2024-11-20 08:21:20 -0500

    Step 0

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanNutsAndBolts
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package "lean-nuts-and-bolts" where
+  -- add package configuration options here
+
+lean_lib «LeanNutsAndBolts» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe "lean-nuts-and-bolts" where
+  root := `Main
